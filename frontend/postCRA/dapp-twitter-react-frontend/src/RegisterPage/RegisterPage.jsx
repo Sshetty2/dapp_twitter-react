02ac5d84a610021fe0175199c6f3d671ce97eea9
Sachit <sachit.shetty@gmail.com>
2019-04-02 15:52:15 -0400

    add formik to registration page

--- frontend/postCRA/dapp-twitter-react-frontend/src/RegisterPage/RegisterPage.jsx
+++ frontend/postCRA/dapp-twitter-react-frontend/src/RegisterPage/RegisterPage.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import { Formik } from 'formik';
+import { Form } from './Form';
 import { userActions } from '../_actions';
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+    name: Yup.string("Enter a name").required("Name is required"),
+    email: Yup.string("Enter your email")
+      .email("Enter a valid email")
+      .required("Email is required"),
+    password: Yup.string("")
+      .min(8, "Password must contain atleast 8 characters")
+      .required("Enter your password"),
+    confirmPassword: Yup.string("Enter your password")
+      .required("Confirm your password")
+      .oneOf([Yup.ref("password")], "Password does not match")
+  });
+
 
 class RegisterPage extends React.Component {
     constructor(props) {
@@ -49,44 +65,12 @@ class RegisterPage extends React.Component {
         const { user, submitted } = this.state;
         return (
             <div className="col-md-6 col-md-offset-3">
-                <h2>Register</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
-                        <label htmlFor="firstName">First Name</label>
-                        <input type="text" className="form-control" name="firstName" value={user.firstName} onChange={this.handleChange} />
-                        {submitted && !user.firstName &&
-                            <div className="help-block">First Name is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.lastName ? ' has-error' : '')}>
-                        <label htmlFor="lastName">Last Name</label>
-                        <input type="text" className="form-control" name="lastName" value={user.lastName} onChange={this.handleChange} />
-                        {submitted && !user.lastName &&
-                            <div className="help-block">Last Name is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.userid ? ' has-error' : '')}>
-                        <label htmlFor="userid">Userid</label>
-                        <input type="text" className="form-control" name="userid" value={user.userid} onChange={this.handleChange} />
-                        {submitted && !user.userid &&
-                            <div className="help-block">Userid is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" value={user.password} onChange={this.handleChange} />
-                        {submitted && !user.password &&
-                            <div className="help-block">Password is required</div>
-                        }
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary">Register</button>
-                        {registering && 
-                            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                        }
-                        <Link to="/login" className="btn btn-link">Cancel</Link>
-                    </div>
-                </form>
+                 <Formik
+                    render={props => <Form {...props} />}
+                    initialValues={user}
+                    validationSchema={validationSchema}
+                    onSubmit = {this.handleSubmit}
+                />
             </div>
         );
     }
@@ -101,3 +85,45 @@ function mapStateToProps(state) {
 
 const connectedRegisterPage = connect(mapStateToProps)(RegisterPage);
 export { connectedRegisterPage as RegisterPage };
+
+
+
+
+{/* <h2>Register</h2>
+<form name="form" onSubmit={this.handleSubmit}>
+    <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
+        <label htmlFor="firstName">First Name</label>
+        <input type="text" className="form-control" name="firstName" value={user.firstName} onChange={this.handleChange} />
+        {submitted && !user.firstName &&
+            <div className="help-block">First Name is required</div>
+        }
+    </div>
+    <div className={'form-group' + (submitted && !user.lastName ? ' has-error' : '')}>
+        <label htmlFor="lastName">Last Name</label>
+        <input type="text" className="form-control" name="lastName" value={user.lastName} onChange={this.handleChange} />
+        {submitted && !user.lastName &&
+            <div className="help-block">Last Name is required</div>
+        }
+    </div>
+    <div className={'form-group' + (submitted && !user.userid ? ' has-error' : '')}>
+        <label htmlFor="userid">Userid</label>
+        <input type="text" className="form-control" name="userid" value={user.userid} onChange={this.handleChange} />
+        {submitted && !user.userid &&
+            <div className="help-block">Userid is required</div>
+        }
+    </div>
+    <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
+        <label htmlFor="password">Password</label>
+        <input type="password" className="form-control" name="password" value={user.password} onChange={this.handleChange} />
+        {submitted && !user.password &&
+            <div className="help-block">Password is required</div>
+        }
+    </div>
+    <div className="form-group">
+        <button className="btn btn-primary">Register</button>
+        {registering && 
+            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+        }
+        <Link to="/login" className="btn btn-link">Cancel</Link>
+    </div>
+</form> */}
