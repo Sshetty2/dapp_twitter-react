8ce607182f05f335a10285dc5adc2174b6047ee9
Sachit Shetty @_@ Sonny <sachit.shetty@gmail.com>
2019-04-04 01:03:23 -0400

    add notes to authentication reducer

--- frontend/postCRA/dapp-twitter-react-frontend/src/_reducers/alert.reducer.js
+++ frontend/postCRA/dapp-twitter-react-frontend/src/_reducers/alert.reducer.js
@@ -1,5 +1,7 @@
 import { alertConstants } from '../_constants';
 
+
+
 export function alert(state = {}, action) {
   switch (action.type) {
     case alertConstants.SUCCESS:
--- frontend/postCRA/dapp-twitter-react-frontend/src/_reducers/authentication.reducer.js
+++ frontend/postCRA/dapp-twitter-react-frontend/src/_reducers/authentication.reducer.js
@@ -1,5 +1,19 @@
 import { userConstants } from '../_constants';
 
+//actions that get dispatched regarding user authentication get funneled through this reducer
+
+// reducers are extremely important to the overall operation flow of the redux pattern because this is where actions are funneled and a subsequent eventd are fired that will ultimately impact the store. For instance if an action get's dispatched from the login page as 'this.props.dispatch(userActions.logout());'
+
+// this code will dispatch an action ; Specifically
+// function logout() {
+//    userService.logout();
+//    return { type: userConstants.LOGOUT };
+//})
+
+// the reducer will catch this action and it will fire the neccessary logic below that will change the state of the application.
+
+// this is the same pattern used in all of the React components
+
 let user = JSON.parse(localStorage.getItem('user'));
 const initialState = user ? { loggedIn: true, user } : {};
 
